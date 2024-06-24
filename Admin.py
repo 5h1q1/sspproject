5480a51168a2e16df3f5fe9fb8642f6bf5ee8692
whati <whatisallowed312@gmail.com>
2024-06-24 15:48:37 +0800

    i put fucking malay

--- Admin.py
+++ Admin.py
@@ -3,7 +3,7 @@ import shelve
 from datetime import *
 class Admin(User):
 
-# elgin test lol no its zikri
+# fucking malay
     def __init__(self, first_name, last_name, email, phone_number, password, gender, age, role, address):
         super().__init__(first_name, last_name, email, phone_number, password, gender, age, role)
         self.__address = address
